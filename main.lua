d898a4cc8de837245b01c2e956d21368db835a79
D10644237061s <44630989+D10644237061s@users.noreply.github.com>
2018-10-31 14:24:24 +0800

    Add files via upload
    我在下面加了一行

--- main.lua
+++ main.lua
@@ -3,30 +3,9 @@
 -- Version: 2.0
 -- Sample code is MIT licensed; see https://www.coronalabs.com/links/code/license
 ---------------------------------------------------------------------------------------
-local topAlignAxis = 200
+local background = display.newImage( "World.jpg" , display.contentCentrtX, display.contentCenterY )
 
-local options1 =
-{
-	text = "The quick brown fox jumped over the lazy dog.",
-	x = 90,
-	width = 128,
-	font = native.systemFont,
-	fontSize = 18,
-}
-local myText1 = display.newText( options1 )
-myText1:setFillColor( 1, 0, 0 )
-myText1.anchorY = 0
-myText1.y = topAlignAxis
-
-local options2 =
-{
-	text = "The quick brown fox jumped over the lazy dog,then jumped back again.",
-	x = 230,
-	width = 120,
-	font = native.systemFont,
-	fontSize = 18,
-}
-local myText2 = display.newText( options2 )
-myText2:setFillColor( 0.6, 0.4, 0.8 )
-myText2.anchorY = 0
-myText2.y = topAlignAxis
+local myText = display.newText( "Hello, World!" , display.contentCenterX, display.contentWidth / 4, native.systemFont, 40 )
+myText:setFillColor( 1, 110/255, 110/255 )
+local myText = display.newText( "hi, World!" , display.contentCenterX, display.contentWidth / 4, native.systemFont, 40 )
+myText:setFillColor( 1, 110/255, 110/255 )
